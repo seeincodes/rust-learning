1142402afb8b221d14ad99860fd58e2a75db40b3
“seeinplays” <seeinplays@gmail.com>
2022-07-27 17:14:24 -0600

    mutating indexes in tuple's

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
-    let tup: (i32, bool, char) = (1, true, 'c');
+    let mut tup: (i32, bool, char) = (1, true, 'c');
 
     println!("tup is : {:?}", tup);
+
+    tup.0 = 10;
     
     println!("tup at index 0 : {}", tup.0);
 
