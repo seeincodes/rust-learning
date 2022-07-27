71ebfa5fb286b963c2bc8b83ff90a5f10de6ebaf
“seeinplays” <seeinplays@gmail.com>
2022-07-27 16:03:33 -0600

    constants

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    let x = 4;
-    println!("x is : {}", x);
+    const SECONDS_IN_MINUTES: u32 = 60;
 
-    let x = "hello";
-    println!("x is : {}", x);
+    println!("In Seconds : {}", SECONDS_IN_MINUTES);
 }
