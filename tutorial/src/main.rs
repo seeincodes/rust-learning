015bd0cb7a4336b2cedc2f8fafdbe88f6b83b723
“seeinplays” <seeinplays@gmail.com>
2022-07-27 16:01:50 -0600

    varibles with different types

--- tutorial/src/main.rs
+++ tutorial/src/main.rs
@@ -2,11 +2,6 @@ fn main() {
     let x = 4;
     println!("x is : {}", x);
 
-    {
-        let x = x - 2;
-        println!("x is : {}", x);
-    }
-
-    let x = x + 1;
+    let x = "hello";
     println!("x is : {}", x);
 }
